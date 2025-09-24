da9d7f5d33bbb5cd52f3b66446ea9a5e11ca4e5f
Tharushika Rukshani <tharushikarukshani10@gmail.com>
2025-09-24 18:44:49 +0530

    Patch/tharushika 2 (#5)
    * vulnerabilities fixed
    
    * added context.xml

---
+++ classes/artifacts/goflow_Web_exploded/WEB-INF/views/error/404.jsp
@@ -0,0 +1,3 @@
+<%@ page isErrorPage="true" %>
+<h1>Oops! Page Not Found</h1>
+<p>The requested page could not be located. Please check the URL or contact support.</p>
--- src/main/java/util/QueryUtil.java
+++ src/main/java/util/QueryUtil.java
@@ -2,6 +2,7 @@ package util;
 
 import java.io.File;
 import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -17,8 +18,14 @@ public class QueryUtil extends CommonUtil {
 
     // This method is used to get the query by id
     public static String queryByID(String id) throws SAXException, IOException, ParserConfigurationException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+        factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+        DocumentBuilder builder = factory.newDocumentBuilder();
         Element element = null;
-        NodeList nodeList = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new File(properties.getProperty("queryPath"))).getElementsByTagName("query");
+        NodeList nodeList = builder.parse(new File(properties.getProperty("queryPath")))
+                .getElementsByTagName("query");
 
         for (int value = 0; value < nodeList.getLength(); ++value) {
             element = (Element) nodeList.item(value);
